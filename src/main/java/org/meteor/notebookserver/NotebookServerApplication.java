8606c08147c6d40b9dda8aa6b6c8a3d8121ca97f
clever_WangErgou <48337433+dmu-wxy@users.noreply.github.com>
2022-07-30 21:38:18 +0800

    项目初始化

---
+++ src/main/java/org/meteor/notebookserver/NotebookServerApplication.java
@@ -0,0 +1,13 @@
+package org.meteor.notebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotebookServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/meteor/notebookserver/NotebookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meteor.notebookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
